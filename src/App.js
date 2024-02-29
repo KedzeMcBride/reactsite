ca7fce9be4d35cfd3b0541bcc97650eecb717dce
KedzeMcbride <Kedzemcbridekum@gmail.com>
2024-02-29 15:30:44 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {MyNavbar} from './parts_of_site/nav';
+import 'bootsrap/dist/css/bootstrap.min.css'
+function App() {
+  return (
+    <div className="App">
+      <nav/>
+    </div>
+  );
+}
+
+export default App;
